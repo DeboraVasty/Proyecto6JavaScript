129e15df4c134e2d8bc25cb141385cdc95dfc7a3
Debora Chacach <vasty0295@gmail.com>
2021-06-24 23:38:07 -0600

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,37 @@
+//constructores 
+function Seguro(marca,year,tipo){
+    this.marca=marca;
+    this.year=year;
+    this.tipo=tipo;
+}
+
+    //interfaz de usuario 
+
+function UI(){
+
+}
+//llena las opciones de los años
+UI.prototype.llenarOpciones=()=>{
+    const max=new Date().getFullYear(),
+          min=max-20;
+     const selectYear=document.querySelector ('#year');
+     for(let i=max; i>min; i--){
+         let option=document.createElement('option');
+         option.value=i;
+         option.textContent=i;
+         selectYear.appendChild(option);
+
+
+     }    
+}
+
+
+//instanciar UI
+const ui=new UI();
+console.log(ui);
+
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+    ui.llenarOpciones();//llena el select con los años
+})
